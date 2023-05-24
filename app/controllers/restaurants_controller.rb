46e72f15c7dc32e118f301161273fca323056721
Anne Bortoli <annebortoli@gmail.com>
2023-05-24 18:25:41 -0300

    add some bootstrap on index.html

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,6 +8,20 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_restaurant
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create] # YOU ONLY NEST IF YOU NEED TO KNOW THE ID OF THE PARENT
   end
 end
